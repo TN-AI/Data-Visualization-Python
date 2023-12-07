bca6fad72b415a2be5b96370ea7fd61af1b768ec
TN-AI <153220939+TN-AI@users.noreply.github.com>
2023-12-07 12:52:18 -0500

    Update datavisual.py

--- datavisual.py
+++ datavisual.py
@@ -2,13 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Using Excel data to generate line plot
-excel_file = 'demo.xlsx'
+#excel_file = 'demo.xlsx'
 # Read the Excel file using pandas
-df = pd.read_excel(excel_file)
+#df = pd.read_excel(excel_file)
 
 # Using csv file to generate line plot.
-#csv_file = 'demo.csv'
-#df = pd.read_csv(csv_file)
+csv_file = 'demo.csv'
+df = pd.read_csv(csv_file)
 
 # Assuming your Excel file has columns 'x' and 'y' for the data
 x = df['x']
